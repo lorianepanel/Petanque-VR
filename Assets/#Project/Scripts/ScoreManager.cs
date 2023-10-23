e16b656726fbfe20d127b134bd04f3b4f06d05e2
Loriane <loriane.panel@gmail.com>
2023-10-23 16:41:30 +0200

    VR settings ok with oculus 2, work on BallManager and UI score

--- Assets/#Project/Scripts/ScoreManager.cs
+++ Assets/#Project/Scripts/ScoreManager.cs
@@ -1,24 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
 
 public class ScoreManager : MonoBehaviour
 {
     GameObject[] balls;
+    Transform goal;
+
+    private TMP_Text scoreText;
+
+    private float minDistance;
+
+    private int score;
 
 
-    // Start is called before the first frame update
     void Start()
     {
         balls = GameObject.FindGameObjectsWithTag("Ball"); 
+        goal = GameObject.FindWithTag("Goal").transform;
+        scoreText = GetComponent<TMP_Text>();
+        score = 0;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        minDistance = Mathf.Infinity;
+
         foreach (GameObject ball in balls)
         {
-            Debug.Log(balls.Length);
+            
+            
+            float distanceFromGoal = Vector3.Distance(ball.transform.position, goal.position);
+            Debug.Log($"{ball.name} : {distanceFromGoal}");
+
+            if(distanceFromGoal < minDistance)
+            {
+                minDistance = distanceFromGoal;
+                // score ++;
+                scoreText.SetText($"Score : {score} point(s) <br>Closest ball from goal : {ball.name} <br>Distance from goal : {minDistance:f} m");
+                
+            }
         }
+
+        
     }
 }
